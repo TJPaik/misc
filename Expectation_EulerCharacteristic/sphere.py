622a1b86b88365d67efa2c81d6bd115f46bbc22c
paiktj <paiktj@gmail.com>
2022-07-06 13:05:08 +0900

    Edit: Fix an error

--- Expectation_EulerCharacteristic/sphere.py
+++ Expectation_EulerCharacteristic/sphere.py
@@ -1,6 +1,6 @@
 import torch
+import numpy as np
 from gudhi.rips_complex import RipsComplex
-from tqdm import tqdm
 
 
 def euler_characteristic(n_points, n_dim, inspect_list):
@@ -8,34 +8,54 @@ def euler_characteristic(n_points, n_dim, inspect_list):
     data /= torch.norm(data, dim=1)[:, None]
     distance_matrix = torch.cdist(data, data, )
     distance_matrix = 2 * torch.arcsin(distance_matrix / 2)
-    assert torch.all(~torch.isnan(distance_matrix))
+    try:
+        assert torch.all(~torch.isnan(distance_matrix))
+        assert torch.all(distance_matrix >= 0)
+    except AssertionError:
+        print('None')
+        return None
 
-    rips = RipsComplex(distance_matrix.numpy())
+    rips = RipsComplex(distance_matrix=distance_matrix.numpy())
     tree = rips.create_simplex_tree(max_dimension=len(data) + 1)
 
     euler_characteristic_list = []
-    euler_characteristic_tmp = 0
     generator = tree.get_filtration()
-    not_done = True
-    for idx, inspect_number in tqdm(enumerate(inspect_list), disable=True):
-        while not_done:
-            try:
-                simp, f_value = next(generator)
-            except StopIteration:
-                not_done = False
-                break
-            if f_value > inspect_number:
-                euler_characteristic_list.append(
-                    euler_characteristic_tmp
-                )
-                tmp = 1 if len(simp) % 2 else -1
-                euler_characteristic_tmp += tmp
-                # print(euler_characteristic, 'h')
-                break
+    # print(inspect_list)
+    tmp = 0
+    # last_summand = 0
+    f = -np.inf
+    for inspect_number in inspect_list:
+        while True:
+            if f <= inspect_number:
+                try:
+                    s, f = next(generator)
+                    # print(f)
+                    s = 1 if len(s) % 2 else -1
+                    tmp += s
+                except StopIteration:
+                    s = 0
+                    f = np.inf
             else:
-                tmp = 1 if len(simp) % 2 else -1
-                euler_characteristic_tmp += tmp
-                # print(euler_characteristic, 'c')
-        if not not_done:
-            euler_characteristic_list.append(euler_characteristic_tmp)
+                euler_characteristic_list.append(tmp - s)
+                break
     return euler_characteristic_list
+
+
+if __name__ == '__main__':
+    from joblib import Parallel, delayed
+    import matplotlib.pyplot as plt
+
+    xs = np.linspace(0, np.pi, 1000)
+    euler_characteristic(2, 2, xs)
+
+    output = Parallel(n_jobs=8, verbose=9)(
+        delayed(euler_characteristic)(10, 2, np.linspace(0, np.pi, 1000))
+        for _ in range(40000)
+    )
+    output = np.asarray(output)
+
+    plt.plot(
+        # xs[:100],
+        output.mean(axis=0)
+    )
+    plt.show()
